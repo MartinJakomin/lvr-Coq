0273e40462ad97c4fe104fea725359343ac6ee71
Martin Jakomin <martin.jakomin@gmail.com>
2014-05-31 23:09:16 +0200

    first attempt on proving

--- sort.v
+++ sort.v
@@ -25,3 +25,55 @@ Eval compute in (insertionSort (12 :: 2 :: 7 :: 46 :: 5 :: 6 :: 7 :: 28 :: 19 ::
 
 
 
+(* The first element in a sorted list is the smallest *)
+Lemma first_element_is_the_smallest (a : Z) (tl : list Z):
+   forall x , In x tl -> urejen (a :: tl) -> (a <= x)%Z.
+Proof.
+   intros.
+   admit.
+Qed.
+
+
+(* Adding bigger element doesnt change upper Lemma *)
+Lemma smaller_than_x (a x : Z) (tl : list Z):
+   urejen (a :: tl) /\ (a <= x)%Z -> forall y, In y (insert x tl) -> (a <= y)%Z.
+Proof.
+   intros.
+   destruct H.
+   admit.
+Qed.
+
+
+(* TODO: Other lemmas you need for insert_is_sorted *)
+
+
+(* Insertion of an element keeps list sorted *)
+Lemma insert_is_sorted (x : Z) (lst : list Z):
+   urejen lst -> urejen (insert x lst).
+Proof.
+   intros.
+   induction lst.
+   - auto.
+   - simpl.
+     case_eq (x <=? a)%Z.
+     + intro.
+       simpl.
+       firstorder.
+       apply Z.leb_le.
+       auto.
+     + intro.
+       admit.       
+Qed.
+
+
+(* InsertionSort always returns sorted list *)
+Theorem sorted (l : list Z):
+   forall l, urejen (insertionSort l).
+Proof.
+   admit.
+Qed.
+
+(* TODO: Other theorems that program works correctly *)
+
+
+
